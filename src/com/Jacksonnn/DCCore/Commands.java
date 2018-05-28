513f73e5fa7511e6c0d42d5e7969c67e958f0532
Jacksonnn <jhuse01@gmail.com>
2018-05-28 10:52:22 -0400

    Bug Fixes and Improvements:  - Updated plugin.yml.  - Removed aliases from registerring commands, will be created when plugin.yml is run.  - Changed broadcast message to: [Darkcrest] message -(player) instead of [Darkcrest] message- (player).  - Changed prefix to [DCCore] + ColorChat + " ".  - Added the start of the ChatSensor.
    ISSUE:
     - Chat Toggle command doesn't work at all (possibly fixed in this commit).

--- src/com/Jacksonnn/DCCore/Commands.java
+++ src/com/Jacksonnn/DCCore/Commands.java
@@ -1,5 +1,6 @@
 package com.Jacksonnn.DCCore;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,18 +17,64 @@ public class Commands implements CommandExecutor {
             sender.sendMessage(GeneralMethods.prefix + "DCCore Help:");
             sender.sendMessage(ChatColor.GOLD + "/dccore chat");
 
-        } else if (args.length == 1 && args[1].equalsIgnoreCase("chat")) {
+        } else if (args.length >= 1 && args[1].equalsIgnoreCase("chat")) {
             if (args[2].equalsIgnoreCase("toggle")) {
 
                 if (sender.hasPermission("DCCore.chat.toggle")) {
                     ChatListener.chatEnabled = !ChatListener.chatEnabled;
-
-                    sender.sendMessage(ChatColor.GREEN + (ChatListener.chatEnabled ? GeneralMethods.prefix + GeneralMethods.successColor + "Unmuted the chat" : GeneralMethods.prefix + GeneralMethods.successColor + "Muted the chat"));
+                    Bukkit.broadcastMessage(ChatListener.chatEnabled ? GeneralMethods.serverPrefix + "Chat has been unmuted." : GeneralMethods.serverPrefix + "Chat has been muted.");
+                    sender.sendMessage(ChatListener.chatEnabled ? GeneralMethods.prefix + GeneralMethods.successColor + "Unmuted the chat." : GeneralMethods.prefix + GeneralMethods.successColor + "Muted the chat.");
                 }
             } else {
-                    sender.sendMessage(GeneralMethods.prefix + GeneralMethods.errorColor + "To use this command, /dccore chat <toggle>.");
+                    sender.sendMessage(GeneralMethods.prefix + GeneralMethods.errorColor + "To use this command, /dccore chat toggle.");
             }
         }
         return true;
     }
 }
+
+/*
+[ERROR] null
+
+org.bukkit.command.CommandException: Unhandled exception executing command 'dc' in plugin DCCore vBETA v1.0
+
+at org.bukkit.command.PluginCommand.execute(PluginCommand.java:46) ~[spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at org.bukkit.command.SimpleCommandMap.dispatch(SimpleCommandMap.java:141) ~[spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at org.bukkit.craftbukkit.v1_12_R1.CraftServer.dispatchCommand(CraftServer.java:651) ~[spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.PlayerConnection.handleCommand(PlayerConnection.java:1386) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.PlayerConnection.a(PlayerConnection.java:1221) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.PacketPlayInChat.a(PacketPlayInChat.java:45) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.PacketPlayInChat.a(PacketPlayInChat.java:1) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.PlayerConnectionUtils$1.run(SourceFile:13) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511) [?:1.8.0_172]
+
+at java.util.concurrent.FutureTask.run(FutureTask.java:266) [?:1.8.0_172]
+
+at net.minecraft.server.v1_12_R1.SystemUtils.a(SourceFile:46) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.MinecraftServer.D(MinecraftServer.java:748) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.DedicatedServer.D(DedicatedServer.java:406) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.MinecraftServer.C(MinecraftServer.java:679) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at net.minecraft.server.v1_12_R1.MinecraftServer.run(MinecraftServer.java:577) [spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+at java.lang.Thread.run(Thread.java:748) [?:1.8.0_172]
+
+Caused by: java.lang.ArrayIndexOutOfBoundsException: 1
+
+at com.Jacksonnn.DCCore.Commands.onCommand(Commands.java:19) ~[?:?]
+
+at org.bukkit.command.PluginCommand.execute(PluginCommand.java:44) ~[spigot-1.12.1.jar:git-Spigot-da42974-8f47214]
+
+... 15 more
+ */
