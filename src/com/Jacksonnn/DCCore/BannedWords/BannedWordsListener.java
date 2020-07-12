3df2e528895e360bc0294e94f2c34badbb946fe9
Jacksonnnnn <jackson.huse@stu.jefferson.kyschools.us>
2020-07-12 01:37:23 -0400

    Correctly updated version... I hope

--- src/com/Jacksonnn/DCCore/BannedWords/BannedWordsListener.java
+++ src/com/Jacksonnn/DCCore/BannedWords/BannedWordsListener.java
@@ -12,22 +12,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BannedWordsListener implements Listener {
+
+    private List<String> bannedWords = new ArrayList<>();
+
     @EventHandler
     public void onRegularChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
 
         bannedWords = ConfigManager.bannedWords.get().getStringList("AntiCurse.bannedWords");
 
+        String detected = " ";
+
         for (String bannedWord : bannedWords) {
             if (event.getMessage().toUpperCase().contains(bannedWord.toUpperCase()) || player.getName().toUpperCase().contains(bannedWord.toUpperCase()) || player.getDisplayName().toUpperCase().contains(bannedWord.toUpperCase())) {
-                if (player.hasPermission("DCCore.AntiCurse.bypass")) {
-                    return;
+                if (bannedWords.size() == bannedWords.indexOf(bannedWord) + 1) {
+                    detected += bannedWord;
                 } else {
-                    event.setCancelled(true);
-                    event.getPlayer().sendMessage(GeneralMethods.prefix + " Please rethink your choice of words... (check your username, nickname, or chat message!!!)");
-                    event.getPlayer().sendMessage(ChatColor.YELLOW + "Banned word used: " + bannedWord);
+                    detected += bannedWord + ", ";
                 }
             }
+        }
+
+        if (!player.hasPermission("DCCore.AntiCurse.bypass")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(GeneralMethods.prefix + " Please rethink your choice of words... (check your username, nickname, or chat message!!!)");
+            event.getPlayer().sendMessage(ChatColor.YELLOW + " " + ChatColor.ITALIC + "Banned word used:" + ChatColor.RESET + detected);
         }
     }
 }
