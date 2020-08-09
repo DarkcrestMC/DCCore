ce5538dcd74ebdec239dfce6569216b5eea8b7a0
Celtican <Celtican5@mail.com>
2020-08-09 17:49:23 -0400

    Added dcguide and bendingguide commands

--- src/com/Jacksonnn/DCCore/Rankup/Ranks.java
+++ src/com/Jacksonnn/DCCore/Rankup/Ranks.java
@@ -17,7 +17,7 @@ public class Ranks implements CommandExecutor {
         FileConfiguration config = ConfigManager.defaultConfig.get();
         sender.sendMessage(GeneralMethods.prefix + ChatColor.of("#E5B100") + "Here are is all of the ranking information!");
         sender.sendMessage(GeneralMethods.prefix + ChatColor.of("#E5B100") + "Use " + ChatColor.AQUA + ChatColor.UNDERLINE +
-                "/warp RankInfo" + ChatColor.RESET + GeneralMethods.accentColor + " to learn about each rank!");
+                "/warp RankInfo" + ChatColor.RESET + ChatColor.of("#E5B100") + " to learn about each rank!");
 //        sender.sendMessage(ChatColor.DARK_BLUE + "Guest " + ChatColor.WHITE + "-> " + ChatColor.GOLD + "Member " + ChatColor.WHITE + "- Complete Rules Check (Forced GUI)");
 //        sender.sendMessage(ChatColor.GOLD + "Member " + ChatColor.WHITE + "-> " + ChatColor.AQUA + "Citizen " + ChatColor.WHITE + String.format("- %s hours & %s coins", config.getInt("Rankup.Hours.Citizen"), config.getInt("Rankup.Prices.Citizen")));
 //        sender.sendMessage(ChatColor.AQUA + "Citizen " + ChatColor.WHITE + "-> " + ChatColor.GREEN + "Merchant " + ChatColor.WHITE + String.format("- %s hours & %s coins", config.getInt("Rankup.Hours.Merchant"), config.getInt("Rankup.Prices.Merchant")));
