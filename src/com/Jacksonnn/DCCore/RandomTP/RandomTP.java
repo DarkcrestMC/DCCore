e3291d123912fb5c2b32653345cefdb9ce838b8a
SaltyGraham <62857144+SaltyGraham@users.noreply.github.com>
2020-11-06 14:48:18 -0500

    Update RandomTP.java
    Reverted Rtp

--- src/com/Jacksonnn/DCCore/RandomTP/RandomTP.java
+++ src/com/Jacksonnn/DCCore/RandomTP/RandomTP.java
@@ -21,7 +21,7 @@ public class RandomTP implements CommandExecutor {
                     sender.sendMessage(GeneralMethods.errorPrefix);
                     return true;
                 }
-                World world = world;
+                World world = player.getWorld();
                 int radius;
 
                 if (ConfigManager.defaultConfig.get().getBoolean("RandomTP.SetRadius")) {
